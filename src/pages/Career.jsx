9020d3ec84a8727ea50901bc70b49efb80d45794
SumanUD <suman.banik.codtrees@gmail.com>
2025-04-10 17:55:17 +0530

    remaining pages

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -9,24 +9,28 @@ const Career = () => {
         <>
             <Navbar />
             <main className="career">
-            <div className="homebanner">
-                <div className="bannerText">
-                <p>Welcome</p>
-                <p>to the <b>Kerovit</b></p>
-                <p><b>team</b></p>
+                <div className="homebanner">
+                    <div className="bannerText">
+                        <p>Welcome</p>
+                        <p>to the <b>Kerovit</b></p>
+                        <p><b>team</b></p>
+                    </div>
+                    <FaWhatsapp className="whatsapp-icon" />
                 </div>
-                <FaWhatsapp className="whatsapp-icon" />
-            </div>
 
-            <div className="belowbanner">
-                <div className="belowBannerText">
-                <img src="/career/belowBannerText.png" alt="belowBannerText" />
-                <p>Send us your resume at</p>
-                <p className="mail">info@kerovit.com</p>
+                <div className="carrerText">
+                    <p>
+                        Our passion is water. Are you a genuine team player? If so, why not make your way over to Hansgrohe, the successful brand-name company and one of the few global players in the sanitation industry. Whether you're still just starting out in your career, whether you want to gain practical experience alongside your studies, or whether you're looking for a new professional home: here, exciting career prospects await.
+                    </p>
                 </div>
-            </div>
 
-            
+                <div className="belowbanner">
+                    <div className="belowBannerText">
+                        <img src="/career/belowBannerText.png" alt="belowBannerText" />
+                        <p>Send us your resume at</p>
+                        <p className="mail">info@kerovit.com</p>
+                    </div>
+                </div>
 
             </main>
             <Footer />
