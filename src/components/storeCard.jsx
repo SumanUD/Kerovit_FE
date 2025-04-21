a5b55d7d004e9d8c347b22ca1d849f6de0852184
siddharth <siddharthmthapa@gmail.com>
2025-04-21 19:58:04 +0530

    locate-our-store design changes

--- src/components/storeCard.jsx
+++ src/components/storeCard.jsx
@@ -15,32 +15,35 @@ const StoreCard = ({
     return (
         <main className="all-store-card">
             <div className="store-card">
-                <img src={storeHeader} alt="Store header" className="storeHeader" />
-                <img src={storeImage} alt="Store preview" className="store-image" />
-                <p className="desc">{description}</p>
+                <div className="store-images">
+                    <img src={storeHeader} alt="Store header" className="storeHeader" />
+                    <img src={storeImage} alt="Store preview" className="store-image" />
+                </div>
+                <div className="store-text-contents">
+                    <p className="desc">{description}</p>
+                    <div className="store-info">
+                        <h3 className="store-name">{storeName}</h3>
+                        <p className="owner-name">{ownerName}</p>
+                        <p className="store-details">{storeDetails}</p>
+                        <p className="store-phone">
+                            <FaPhoneAlt /> {storePhone}
+                        </p>
 
-                <div className="store-info">
-                    <h3 className="store-name">{storeName}</h3>
-                    <p className="owner-name">{ownerName}</p>
-                    <p className="store-details">{storeDetails}</p>
-                    <p className="store-phone">
-                        <FaPhoneAlt /> {storePhone}
-                    </p>
-
-                    <div className="actions">
-                        <a href={directionUrl} target="_blank" rel="noopener noreferrer">
-                            <button className="direction-btn">
-                                Get Direction{" "}
-                                <img
-                                    src="/locate-our-store/arrow-top-right.png"
-                                    alt="arrow-top-right"
-                                    className="arrow-top-right"
-                                />
-                            </button>
-                        </a>
-                        <button type="button" className="swipe-btn">
-                            Swipe <BsArrowRight className="right_arrow" />
-                        </button>
+                        <div className="actions">
+                            <a href={directionUrl} target="_blank" rel="noopener noreferrer">
+                                <button className="direction-btn">
+                                    Get Direction{" "}
+                                    <img
+                                        src="/locate-our-store/arrow-top-right.png"
+                                        alt="arrow-top-right"
+                                        className="arrow-top-right"
+                                    />
+                                </button>
+                            </a>
+                            {/* <button type="button" className="swipe-btn">
+                                Swipe <BsArrowRight className="right_arrow" />
+                            </button> */}
+                        </div>
                     </div>
                 </div>
             </div>
